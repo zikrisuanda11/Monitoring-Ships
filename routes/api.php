1ff1657b139ed4c775ef37447416f7da812ecb81
zikrisuanda11 <zikrisuanda998@gmail.com>
2022-12-20 10:19:40 +0800

    add middleware

--- routes/api.php
+++ routes/api.php
@@ -22,16 +22,14 @@ use App\Http\Controllers\CorporateController;
 //     return $request->user();
 // });
 
-Route::apiResource('container', ContainerController::class);
-Route::apiResource('corporate', CorporateController::class);
-Route::apiResource('user', UserController::class);
-
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     
+    Route::apiResource('container', ContainerController::class);
+    Route::apiResource('corporate', CorporateController::class);
+    Route::apiResource('user', UserController::class);
+
     Route::post('logout', [AuthController::class, 'logout']);
-    // API route for logout user
-    // Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
 });
