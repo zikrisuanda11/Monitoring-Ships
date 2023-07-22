f915a16327cb1be948ac65a8ea9d29162a8254e9
zikrisuanda11 <zikrisuanda998@gmail.com>
2023-07-22 14:45:04 +0800

    update: db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        URL::forceScheme('https');
         Inertia::share('appUrl', function () {
             return URL::to('/');
         });
